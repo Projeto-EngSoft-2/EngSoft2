1db628ab515552f16a51a9360449b54165f4b38a
Gabriel Vieira <gabrielvieira040@outlook.com>
2025-02-03 09:21:23 -0300

    feat: Adicionado pagina de Registro

--- Front/EngSoft2/src/App.jsx
+++ Front/EngSoft2/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "./components/HomePage/HomePage.jsx";
+import LoginPage from "./components/RegisterPage/RegisterPage.jsx"
 import "./App.css";
+
 
 export default function App() {
     return (
         <div className="App">
-            <HomePage />
+            <LoginPage />
         </div>
     );
 }
