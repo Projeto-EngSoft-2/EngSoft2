f7548b290718cdb28e504ba3ddc8954b5b326792
AntonioRian <rian.crato@gmail.com>
2025-02-04 23:46:35 -0300

    feat: adicionado rotas e funcionalidades do admin

--- Back/src/routes/auth.js
+++ Back/src/routes/auth.js
@@ -10,15 +10,24 @@ router.post('/', async (req, res) => {
   try {
     const user = await User.findOne({ email });
 
-    
     if (!user || !(await bcrypt.compare(senha, user.senha))) {
       return res.status(401).json({ message: 'Credenciais inválidas.' });
     }
 
-    const payload = { email };
+    const payload = { 
+      email: user.email,
+      isAdmin: user.isAdmin 
+    };
+    
     const token = jwt.encode(payload, process.env.JWT_SECRET);
 
-    res.json({ token });
+    res.json({ 
+      token,
+      user: {
+        email: user.email,
+        isAdmin: user.isAdmin
+      }
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Erro interno no servidor.' });
